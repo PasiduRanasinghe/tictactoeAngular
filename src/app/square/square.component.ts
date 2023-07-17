ea013f4eea45fcbfc81b154d0ed4fb248cbc12c6
Pasidu Prabhath <104724063+PasiduPrabhath@users.noreply.github.com>
2023-07-17 12:57:33 +0530

    ui devoloped with new colors

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,11 +4,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-      <button mat-flat-button  *ngIf="!value">{{value}}</button>
-      <button mat-flat-button style="color:red;"*ngIf="value=='X'">{{value}}</button>
-      <button mat-flat-button style="color:yellow;" *ngIf="value=='O'">{{value}}</button>
+      <button *ngIf="!value">{{value}}</button>
+      <button style="color:white;"*ngIf="value">{{value}}</button>
+    
   `,
-  styles: ['button { width: 100% !important; height: 100% !important; font-size: 4.5em !important; margin:0.2rem;}']
+  styles: ['button { width: 100% !important; height: 100% !important; font-size: 4.7em !important; background:rgb(46, 46, 46); border: none;}']
 })
 export class SquareComponent {
   @Input()
